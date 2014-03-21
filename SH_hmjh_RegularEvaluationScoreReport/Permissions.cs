4a6d7576fb3ddae3c1f175b12e2e129cfe97fc20
chentaichen <ct.chen@ischool.com.tw>
2014-03-21 14:48:46 +0800

    和美_定期評量成績單_專案檔(epost)

---
+++ SH_hmjh_RegularEvaluationScoreReport/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SH_hmjh_RegularEvaluationScoreReport
+{
+    class Permissions
+    {
+        public static string KeyHmjhRegularEvaluationScoreReport { get { return "SHSchool.SH_hmjh_RegularEvaluationScoreReport.cs"; } }
+        public static bool IsEnableHmjhRegularEvaluationScoreReport
+        {
+            get
+            {
+                return FISCA.Permission.UserAcl.Current[KeyHmjhRegularEvaluationScoreReport].Executable;
+            }
+        }
+    }
+}
